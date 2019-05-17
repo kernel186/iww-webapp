109ecdf10b672c0bec5c059ab9f1dea3455db5ab
kernel186 <yulius.leonaro@yahoo.com>
2019-05-17 18:12:44 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get 'sessions/new'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get 'welcome/index', to: 'welcome#index'
+  resources :users
+end
